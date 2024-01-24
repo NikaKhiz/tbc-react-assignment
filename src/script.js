ca0a7d93c17e352ac605beadb31ac685af4ef93e
NikaKhiz <khizambarelinika123@gmail.com>
2024-01-24 23:32:29 +0400

    feat: header functionalities sticky header on scroll mobile menu opening functionality

--- src/script.js
+++ src/script.js
@@ -1,1 +1,24 @@
 import "./style.scss";
+
+const hamburgerButton = document.querySelector(".hamburger");
+const mobileMenu = document.querySelector(".mobile__menu");
+const mobileNavigation = document.querySelector(".mobile__menu__navigation");
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+
+// open mobile menu
+hamburgerButton.addEventListener("click", () => {
+  hamburgerButton.classList.toggle("open");
+  mobileMenu.classList.toggle("is__menu__open");
+  mobileNavigation.classList.toggle("is__menu__open");
+  body.classList.toggle("noscroll");
+});
+
+// sticky header on scroll
+document.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+});
