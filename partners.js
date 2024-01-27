b69d4dfc5f085310ec030800eef79a510c2d164e
NikaKhiz <khizambarelinika123@gmail.com>
2024-01-28 01:46:11 +0400

    style: partners carousel

---
+++ partners.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    title: "partner space",
+    image: "./src/assets/images/partner-space.webp",
+  },
+  {
+    title: "partner spectre",
+    image: "./src/assets/images/partner-spectre.webp",
+  },
+  {
+    title: "partner tbc lising",
+    image: "./src/assets/images/partner-tbc-lising.webp",
+  },
+  {
+    title: "partner tegeta",
+    image: "./src/assets/images/partner-tegeta.webp",
+  },
+  {
+    title: "partner tnet",
+    image: "./src/assets/images/partner-tnet.webp",
+  },
+  {
+    title: "partner usaid",
+    image: "./src/assets/images/partner-usaid.webp",
+  },
+  {
+    title: "partner ufc",
+    image: "./src/assets/images/partner-ufc.webp",
+  },
+];
